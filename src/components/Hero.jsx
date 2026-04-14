392522070764b8674483a07b236fc8ad0df40d09
Sumanth Angadi <sumanthangadi7@gmail.com>
2026-04-14 18:16:09 +0530

    Reduce scale of background ordered.png to 105%

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ export function Hero() {
                             <img
                                 src={borderedImage}
                                 alt="Background effect"
-                                className="absolute w-full h-auto object-cover object-top z-0 scale-110 translate-x-4 -translate-y-4"
+                                className="absolute w-full h-auto object-cover object-top z-0 scale-105 translate-x-4 -translate-y-4"
                             />
                             <img
                                 src={profileImage}
@@ -73,7 +73,7 @@ export function Hero() {
                         <img
                             src={borderedImage}
                             alt="Background effect"
-                            className="absolute w-full h-auto object-cover object-top z-0 scale-110 translate-x-4 -translate-y-4"
+                            className="absolute w-full h-auto object-cover object-top z-0 scale-105 translate-x-4 -translate-y-4"
                         />
                         <img
                             src={profileImage}
